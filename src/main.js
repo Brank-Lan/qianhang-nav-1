92bbc1c31105c92174dd175d06274e1695db8741
âBrank-Lan <2055929044@qq.com>
2021-03-09 18:33:31 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -2,32 +2,39 @@ const $siteList = $('.siteList')
 const $lastLi = $siteList.find('li.last')
 const x = localStorage.getItem('x')
 const xObject = JSON.parse(x)
-const hashMap =xObject || [
-    {
-        logo:'https://www.google.com/s2/favicons?sz=64&domain_url=acfun.cn',
-        logoType:'text',
-        url:'https://www.acfun.cn'
-    },{
-        logo:'https://www.google.com/s2/favicons?sz=64&domain_url=bilibili.com',
-        logoType:'image',
-        url:'https://bilibili.com',
-    }
-]
+const hashMap = xObject || [{
+    logo: 'J',
+    logoType: 'text',
+    url: 'https://juejin.cn'
+}, {
+    logo: 'B',
+    logoType: 'image',
+    url: 'https://bilibili.com',
+}, {
+    logo: 'V',
+    logoType: 'image',
+    url: 'https://cn.vuejs.org/index.html',
+}, {
+    logo: 'Y',
+    logoType: 'image',
+    url: 'https://www.yuque.com/dashboard',
+}]
 
-const simplifyUrl=(url)=>{
-    return url.replace('https://','')
-    .replace('http://','')
-    .replace('www.','')
-    .replace(/\/.*/,'') //删除/开头的内容
+const simplifyUrl = (url) => {
+    return url.replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .replace('cn.', '')
+        .replace(/\/.*/, '') //删除/开头的内容
 }
-const render = ()=>{
+const render = () => {
     $siteList.find('li:not(.last)').remove()
-    hashMap.forEach((node,index)=>{
+    hashMap.forEach((node, index) => {
 
         let $li = $(`<li>
                     <div class="site">
                         <div class="logo">
-                            <img src="${node.logo}" alt="" sizes="64x64">
+                            ${node.logo}
                         </div>
                         <div class="link">${simplifyUrl(node.url)}</div>
                         <div class="close">
@@ -37,12 +44,12 @@ const render = ()=>{
                         </div>
                     </div>
                 </li>`).insertBefore($lastLi)
-        $li.on('click',(e)=>{
+        $li.on('click', (e) => {
             window.open(node.url)
         })
-        $li.on('click','.close',(e)=>{
-            e.stopPropagation()     //阻止冒泡
-            hashMap.splice(index,1)
+        $li.on('click', '.close', (e) => {
+            e.stopPropagation() //阻止冒泡
+            hashMap.splice(index, 1)
             render()
         })
     })
@@ -50,32 +57,35 @@ const render = ()=>{
 
 render()
 
-$('.addButton').on('click',()=>{
+$('.addButton').on('click', () => {
     let url = window.prompt('请问你要添加的网址是啥')
-    if(url !== '' && url !== null){
-        if(url.indexOf('http') !== 0){
-            url = 'https://'+url
+    if (url !== '' && url !== null) {
+        if (url.indexOf('http') !== 0) {
+            url = 'https://' + url
         }
-        let logo = `https://www.google.com/s2/favicons?sz=64&domain_url=${simplifyUrl(url)}`
+        let logo = simplifyUrl(url)[0].toUpperCase()
+        console.log('logo', logo)
         hashMap.push({
-            logo:logo,
-            logoType:'text',
-            url:url
+            logo: logo,
+            logoType: 'text',
+            url: url
         })
         render()
     }
 })
 
-window.onbeforeunload=()=>{
+window.onbeforeunload = () => {
     const string = JSON.stringify(hashMap)
-    localStorage.setItem('x',string)
+    localStorage.setItem('x', string)
 }
 
-$(document).on('keypress',(e)=>{
-    const {key} = e
-    for(let i=0;i<hashMap.length;i++){
+$(document).on('keypress', (e) => {
+    const {
+        key
+    } = e
+    for (let i = 0; i < hashMap.length; i++) {
         let logo = simplifyUrl(hashMap[i].url)[0]
-        if(logo.toLowerCase() === key){
+        if (logo.toLowerCase() === key) {
             window.open(hashMap[i].url)
         }
     }
